bd777eb95d9e8fba0bdc8eb717aa875921d5f1ec
Gaurav Sonar <boyonjourney29@gmail.com>
2026-08-17 18:28:35 +0530

    First Commit

--- cypress/e2e/Organizationsetup/e2e-002-nonsole-essential.cy.js
+++ cypress/e2e/Organizationsetup/e2e-002-nonsole-essential.cy.js
@@ -1,4 +1,4 @@
-import GetStartedPage from '../../pages/OrganizationSetup/GetStartedPage';
+import GetStartedPage from '../../pages/OrganizationSetup/GetstartedPage';
 import CreateorgProfile from '../../pages/OrganizationSetup/CreateorgProfile';
 import PromocodePage from '../../pages/OrganizationSetup/PromocodePage';
 import PaymentPage from '../../pages/OrganizationSetup/PaymentPage';
